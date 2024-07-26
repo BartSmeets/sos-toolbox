76cc8ff2625790a568059fcca238c6d3ade245ce
Bart Smeets <bart.smeets.qsp@kuleuven.be>
2024-07-26 12:01:49 +0200

    App

--- pages/2_🌡️_Black_Body.py
+++ pages/2_🌡️_Black_Body.py
@@ -1,44 +1,69 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.set_page_config(
         page_title="Summer of Science",
         page_icon="☀️")
 
 # Constants
-h = 4.135667696e-15     # Planck's constant (eV/Hz)
-c = 2.99792458e17       # Speed of Light (nm/s)
-k = 8.617343e-5    # Boltzmann Constant (eV/K)
+h = 6.62607015e-34     # Planck's constant (J/s)
+c = 2.99792458e8       # Speed of Light (m/s)
+k = 1.380649e-23    # Boltzmann Constant (J/K)
 converter = lambda x: h*c/x
 Si_bandgap = 1.14   # eV
 
 # Initialise session state
-if 'bd-wavelength' not in st.session_state:
-    st.session_state['bd-wavelength']  = converter(Si_bandgap)
-if 'bd-energy' not in st.session_state:
-    st.session_state['db-energy'] = Si_bandgap
-if 'bd-T' not in st.session_state:
-    st.session_state['bd-T'] = 300
+if 'bb-T' not in st.session_state:
+    st.session_state['bb-T'] = 300
 
 # Define callbacks
-def callback1():
-    wavelength = st.session_state['bd-wavelength']
-    st.session_state['bd-energy'] = converter(wavelength)
-def callback2():
-    energy = st.session_state['bd-energy']
-    st.session_state['bd-wavelength'] = converter(energy)
+def Planck(wavelength, T):
+    wavelength = wavelength * 1e-9
+    pre_factor = (8*np.pi*h*c)/(wavelength**5)
+    boltzmann_factor = (np.exp((h*c/wavelength)/(k*T)) - 1)
+    return pre_factor/boltzmann_factor * 1e-9
+
 
 st.title('Black Body Radiation')
 
 # Dashboard
-st.title('Colour Converter')
 col1, col2 = st.columns(2)
 with col1:
-    with st.container(height=350):
+    height = 175
+    with st.container(height=height):
         st.header('Input')
-        st.number_input('Wavelength (nm)', min_value=10, max_value=10000, key='bd-wavelength', on_change=callback1)
-        st.number_input('Energy (eV)', min_value=converter(10000), max_value=converter(10), key='bd-energy', on_change=callback2)
-        st.number_input('Temperature (K)', min_value=4, key='bd-T')
+        st.number_input('Temperature (K)', min_value=4, max_value=10000, key='bb-T')
+        wavelength = np.linspace(200, 1500, 5000)
+        spectrum = Planck(wavelength, st.session_state['bb-T'])
+        if st.session_state['bb-T'] >= 2000:
+            wave_max = wavelength[np.argmax(spectrum)]
+        else:
+            wave_max = 'Out of Range'
+        
+
+with col2:
+    with st.container(height=height):
+        st.header('Output')
+        try:
+            st.write('$\lambda_{max}$ = %.d nm' % wave_max)
+        except TypeError:
+            st.write('$\lambda_{max}$ = Out of Range')
+
+
+fig = plt.figure()
+
+if st.session_state['bb-T'] >= 2000:
+    plt.axvline(wave_max, c='grey', ls=':')
+plt.plot(wavelength, spectrum)
+plt.xticks(np.arange(200, 1501, 100), minor=True)
+plt.axvline(380, c='violet', alpha=0.5)
+plt.axvline(740, c='red', alpha=0.5)
+plt.xlabel('Wavelength (nm)')
+plt.ylabel(r'Spectral Energy Density (W / (m$^2 \cdot$nm))')
+
+st.pyplot(fig)
+
 
 
 
